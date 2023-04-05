b1dc6fa50d0a7869c2979e5e98f3e298830abea9
PARK SEONG HYEON <lovehyun95@gmail.com>
2023-04-05 09:58:59 +0900

    2023.04.05

---
+++ Day02/Day02App/CS10_operator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS10_operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // << == *2 / >> == /2
+            // 비트연산 - 한칸 왼쪽으로 밀면 2배 오른쪽으로 밀면 1/2배
+            int firstval = 15; // 15 = 0x1111
+            int secondval = firstval << 1; // 2진수 0001 1110
+            Console.WriteLine(secondval);
+
+            // 1111 & 1101 = > 1101 15 & 13 = > 13
+            // 1010 | 0101 = > 1111
+
+            firstval = 15;
+            secondval = 13;
+            Console.WriteLine(firstval & secondval);
+            firstval = 10;
+            secondval = 5;
+            Console.WriteLine(firstval | secondval);
+            Console.WriteLine(firstval ^ secondval); // XOR
+            Console.WriteLine(~secondval); // 보수
+            // 실무에서는 많이 안씀
+
+            // Null 병합 연산자
+            int? checkval = null;
+            Console.WriteLine(checkval == null? 0 : checkval); // 3항 연산자
+            Console.WriteLine(checkval ?? 0); // null 병합연산자는 3항 연산자를 더 축소
+
+            checkval = 25; // 비교 할 수 있는 방법은 여러 개
+            Console.WriteLine(checkval.HasValue ? checkval.Value : 0); // HasValue, Value
+            Console.WriteLine(checkval ?? 0);
+        }
+    }
+}
---
+++ Day02/Day02App/CS11_logiccondition/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS11_logiccondition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 20;
+
+            if (a == 20) // 처리 해야되는 로직이 있으면 무조건 {} 써라
+            {
+                // if 문 밑에 문장이 하나 밖에 없으면 중괄호 없이 이렇게 써도 되긴하는데
+                Console.WriteLine("20입니다."); // 초보는 이렇게 쓰지마라
+            }
+            else
+            {
+                Console.WriteLine("20이 아닙니다.");
+            }
+            if (a == 30) return; // 메서드를 완전히 빠져나가는 구문은 이렇게 써도 된다.
+            if (a == 40) // 위애 애랑 똑같은 뜻
+                return;
+        }
+    }
+}
