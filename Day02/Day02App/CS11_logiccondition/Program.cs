f6d443d394b7ad1a8773fd5963f4addb3ce0b5b9
PARK SEONG HYEON <lovehyun95@gmail.com>
2023-04-05 12:31:24 +0900

    2023.04.05 오전 수업끝

--- Day02/Day02App/CS11_logiccondition/Program.cs
+++ Day02/Day02App/CS11_logiccondition/Program.cs
@@ -10,6 +10,9 @@ namespace CS11_logiccondition
     {
         static void Main(string[] args)
         {
+            // #region 제목 
+            // #endregion
+            #region < If 구문 > 
             int a = 20;
 
             if (a == 20) // 처리 해야되는 로직이 있으면 무조건 {} 써라
@@ -24,6 +27,72 @@ namespace CS11_logiccondition
             if (a == 30) return; // 메서드를 완전히 빠져나가는 구문은 이렇게 써도 된다.
             if (a == 40) // 위애 애랑 똑같은 뜻
                 return;
+            #endregion
+
+            #region < 데이터 타입 비교 Switch 구문 >
+            // 데이터 타입 비교 switch문 (C# 7.0부터 .NET framework 4.7 / 4.8)
+            object obj = null;
+
+            string inputs = Console.ReadLine(); // 콘솔에서 입력
+            if (int.TryParse(inputs, out int iouput)) // 예외가 발생하면 0
+            {
+                obj = iouput; // 입력한 값이 정수라서 문자열을 정수로 변환
+            }
+            else if (float.TryParse(inputs, out float foutput))
+            {
+                obj = foutput; // 입력한 값이 실수라서 문자열을 실수로 변환
+            }
+            else
+            {
+                obj = inputs; // 이도 저도 아니다.
+            }
+
+            Console.WriteLine(obj);
+
+            switch (obj) 
+            {
+                case int i: // 지금 들어온 값이 정수라면
+                    Console.WriteLine("{0}는 int 형식입니다.", i);
+                    break;
+                case float f: // 들어온 값이 실수라면
+                    Console.WriteLine("{0}는 float 형식입니다.", f);
+                    break;
+                case string s: // 들어온 값이 문자열이면
+                    Console.WriteLine("{0}는 string 형식입니다.", s);
+                    break;
+                default: // 이외 나머지
+                    Console.WriteLine("몰?루");
+                    break;
+            }
+            #endregion
+
+            #region < 이중 For문 >
+
+            for (int x = 2; x <= 9; x++)
+            {
+                for (int y = 1; y <= 9; y++)
+                {
+                    Console.WriteLine("{0} x {1} = {2}", x, y, x * y);
+                }
+            }
+
+            #endregion
+
+            #region < Foreach 문 >
+            // C#은 배열이랑 리스트랑 아예 다르다.
+            int[] ary = { 2, 4, 6, 8, 10 };
+            // for 문을 써도 된다.
+            foreach (int i in ary) // 배열이나 컬렉션(리스트) 
+            {
+                Console.WriteLine("{0}^2 = {1}", i, i*2);
+            }
+            // for 문보다 훨씬 쓰기 편하기 때문에 쓰는거다.
+            for (int i = 0; i < ary.Length; i++)
+            {
+                Console.WriteLine("{0}^2 = {1}", ary[i], ary[i] * 2);
+            }
+
+            #endregion
         }
     }
 }
---
+++ Day02/Day02App/CS12_methods/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS12_methods
+{
+    class Calculator
+    {
+        // static(정적) - 최초 실행에 메모리 올라감.
+        // 프로그램이 시작하자마자 메모리에 올라간다.
+        // 클래스의 객체를 만들 필요가 없음. 언제든지 접근 가능 like new Calc();
+        public static int Plus(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Minus(int a, int b)
+        { 
+            return a - b; 
+        }
+
+    }
+    internal class Program
+    {
+        /// <summary>
+        /// 실행시 최초 메모리에 올라가야 되기 때문에 static이 되어야하고
+        /// 메서드 이름이 Main 이면 실행될 때 알아서 제일 처음에 시작된다.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            #region < static 메서드 >
+
+            int result = Calculator.Plus(1, 2);
+            // static이 아니면 이렇게 되야한다. 동적 할당 heap(힙) 영역.
+            // int result = new Calculator().Plus(1, 2);
+            // Calc.Minus(3, 2); // Minus는 static이 아니기 때문에 접근 불가(객체생성해야 접근가능)
+            result = new Calculator().Minus(3, 2);
+            Console.WriteLine(result);
+            #endregion
+
+            #region < Call by Reference vs Call by Value 비교 >
+            int x = 10; int y = 3;
+            // ref가 없으면 Call By Value
+            Swap(ref x, ref y); // x, y가 가지고 있는 주소를 전달 Call By Reference == pointer
+
+            Console.WriteLine("x = {0}, y = {1}", x, y);
+
+            Console.WriteLine(GetNum());
+            #endregion
+
+            #region < out 매개변수 >
+
+            int divid = 10;
+            int divor = 3;
+            int rem= 0;
+            Divide(divid, divor, out result, out rem); // ref를 쓰든 out을 쓰든 똑같다.
+            Console.WriteLine("나누기 값 : {0}, 나머지 : {1}", result, rem);
+
+            (result, rem) = Divide(20, 6);
+            Console.WriteLine("나누기 값 : {0}, 나머지 : {1}", result, rem);
+
+            #endregion
+
+            #region < 가변길이 매개변수 >
+
+            Console.WriteLine(Sum(1, 3, 5, 7, 9));
+
+            #endregion
+
+        }
+
+        //static int Divide(int x, int y)
+        //{
+        //    return x / y;
+        //}
+        //static int Reminder(int x, int y)
+        //{
+        //    return x % y;
+        //}
+        // 메서드 두개 만들걸 아래처럼 하나로
+        //static void Divide(int x, int y, ref int val, ref int rem)
+        static void Divide(int x, int y, out int val, out int rem)
+        {
+            val = x / y;
+            rem = x % y;
+        }
+        // 둘 다 똑같음 아래는 튜플
+        static (int result, int rem) Divide(int x, int y)
+        {
+            return (x / y, x % y); // C# 7.0
+        }
+
+        static (float result, int rem) Divide(float x, float y)
+        {
+            return (x / y, (int)(x % y)); // C# 7.0
+        }
+        // Main 메서드랑 같은 레벨에 있는 메서드들은 public은 안되고
+        // 전부 static이 되어야 한다. ( 무조건 )
+
+        // 단일 포인터 - 1차원 배열
+        // 2중 포인터 - 2차원 배열
+        // 3중 포인터 - 3차원 배열
+        public static void Swap(ref int a, ref int b)
+        {
+            int temp = 0;
+            temp = a; // 5 : temp = 5
+            a = b; // 4 : a = 4
+            b = temp; // 5
+        }
+
+        static int val = 100;
+        // static메서드에 접근할 수 있는 변수는 static 밖에 없다.
+        public static ref int GetNum()
+        {
+            return ref val;
+        }
+        public static int Sum(params int[] args) // Python 가변길이 매개변수 (*args) 랑 비교
+        {
+            int sum = 0;
+
+            foreach(var item in args)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+    }
+}
