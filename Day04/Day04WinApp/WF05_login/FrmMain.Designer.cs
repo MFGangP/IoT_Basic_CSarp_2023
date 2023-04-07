54749eecc78706804bb68f6c798918aaf86477f7
PARK SEONG HYEON <lovehyun95@gmail.com>
2023-04-07 17:13:29 +0900

    2023.04.07 오후 수업 끝

---
+++ Day04/Day04WinApp/WF05_login/FrmMain.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WF05_login
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtnLogin = new System.Windows.Forms.Button();
+            this.TxtID = new System.Windows.Forms.TextBox();
+            this.TxtPassWord = new System.Windows.Forms.TextBox();
+            this.LabSuccess = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "아이디";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "패스워드";
+            // 
+            // BtnLogin
+            // 
+            this.BtnLogin.Location = new System.Drawing.Point(156, 108);
+            this.BtnLogin.Name = "BtnLogin";
+            this.BtnLogin.Size = new System.Drawing.Size(75, 23);
+            this.BtnLogin.TabIndex = 3;
+            this.BtnLogin.Text = "로그인";
+            this.BtnLogin.UseVisualStyleBackColor = true;
+            this.BtnLogin.Click += new System.EventHandler(this.BtnLogin_Click);
+            // 
+            // TxtID
+            // 
+            this.TxtID.Location = new System.Drawing.Point(69, 8);
+            this.TxtID.Name = "TxtID";
+            this.TxtID.Size = new System.Drawing.Size(162, 21);
+            this.TxtID.TabIndex = 1;
+            // 
+            // TxtPassWord
+            // 
+            this.TxtPassWord.Location = new System.Drawing.Point(69, 58);
+            this.TxtPassWord.Name = "TxtPassWord";
+            this.TxtPassWord.Size = new System.Drawing.Size(162, 21);
+            this.TxtPassWord.TabIndex = 2;
+            this.TxtPassWord.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtPassWord_KeyPress);
+            // 
+            // LabSuccess
+            // 
+            this.LabSuccess.AutoSize = true;
+            this.LabSuccess.Location = new System.Drawing.Point(12, 162);
+            this.LabSuccess.Name = "LabSuccess";
+            this.LabSuccess.Size = new System.Drawing.Size(0, 12);
+            this.LabSuccess.TabIndex = 4;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(251, 208);
+            this.Controls.Add(this.LabSuccess);
+            this.Controls.Add(this.TxtPassWord);
+            this.Controls.Add(this.TxtID);
+            this.Controls.Add(this.BtnLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.Text = "로그인";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BtnLogin;
+        private System.Windows.Forms.TextBox TxtID;
+        private System.Windows.Forms.TextBox TxtPassWord;
+        private System.Windows.Forms.Label LabSuccess;
+    }
+}
+
---
+++ Day04/Day04WinApp/WF05_login/FrmMain.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WF05_login
+{
+
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+            LabSuccess.Text = "환영합니다.";
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            if (TxtID.Text.Length != 0 && TxtPassWord.Text.Length != 0)
+            {
+                if (TxtID.Text == "abcd" && TxtPassWord.Text == "1234")
+                {
+                    LabSuccess.Text = "로그인 성공";
+                    // KeyPress 이벤트 넣으면
+                }
+                else
+                {
+                    LabSuccess.Text = "로그인 실패";
+                }
+            }
+            else
+            {
+                LabSuccess.Text = "아이디 비밀번호를 입력해주세요.";
+            }
+        }
+
+        private void TxtPassWord_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                BtnLogin_Click (sender, e);
+            }
+        }
+    }
+}
