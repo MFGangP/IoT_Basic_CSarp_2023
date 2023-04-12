d5fd815604b62f3f85059267807efec5e3a10f6c
PARK SEONG HYEON <lovehyun95@gmail.com>
2023-04-12 12:33:51 +0900

    2023.04.12 오전 수업 끝

---
+++ Day07/Day07ConsoleApp/CS26_delegate/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS26_delegate
+{
+    // 대리자 사용하겠다는 선언
+    // 매개 변수는 원래 있는 매개 변수랑 동일하게 써준다.
+    delegate int CalcDelegate(int a, int b);
+
+    delegate int Compare(int a, int b); // a, b 중 뭐가 큰 지(작은 지) 비교 대리자
+
+    class Calc
+    #region < 대리자 학습 기본 >
+    {
+        public int Plus(int a, int b)
+        {
+            return a + b;
+        }
+        // static이 붙으면 무조건 실행 될 때 최초 메모리에 올라감
+        // 프로그램 실행중에는 언제든지 접근 가능
+        // 대신 private는 될 수 없다.
+        public static int Minus(int a, int b) // 시작 할 때 메모리에 올라감
+        {
+            return a - b;
+        }
+    }
+    #endregion
+    internal class Program
+    {
+        static int AscendCompare(int a, int b)
+        {
+            if (a > b) { return 1; }
+            else if (a == b) { return 0; }
+            else return -1;
+        }
+
+        static int DescendCompare(int a, int b)
+        {
+            if (a < b) { return 1; }
+            else if (a == b) { return 0; }
+            else return -1;
+        }
+
+        // 오름차순, 내림차순 정렬 하나의 메서드에서 정리가능
+        static void BubbleSort(int[] DataSet, Compare comparer )
+        {
+            int i = 0, j = 0, temp = 0;
+
+            for ( i = 0; i < DataSet.Length - 1; i++ )
+            {
+                for (j = 0; j < DataSet.Length - (i + 1); j++ )
+                {
+                    if (comparer(DataSet[j], DataSet[j+1]) > 0) // 대리자 사용
+                    {
+                        temp = DataSet[j + 1];
+                        DataSet[j + 1] = DataSet[j];
+                        DataSet[j] = temp; // Swap
+                    }
+                }
+            }
+        }
+
+        static void BubbleSort2(int[] DataSet, bool isAscend)
+        {
+            int i = 0, j = 0, temp = 0;
+
+            for (i = 0; i < DataSet.Length - 1; i++)
+            {
+                for (j = 0; j < DataSet.Length - (i + 1); j++)
+                {
+                    if (isAscend == true) // 대리자 사용
+                    {
+                        if (DataSet[j] > DataSet[j + 1])
+                        {
+                            temp = DataSet[j + 1];
+                            DataSet[j + 1] = DataSet[j];
+                            DataSet[j] = temp; // Swap
+                        }
+                    }
+                    else
+                    {
+                        if (DataSet[j] < DataSet[j + 1])
+                        {
+                            temp = DataSet[j + 1];
+                            DataSet[j + 1] = DataSet[j];
+                            DataSet[j] = temp; // Swap
+                        }
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            #region < 대리자 기본 예 >
+            // 일반적으로 클래스 사용하는 방식 - 직접 호출
+            Calc nomalCalc = new Calc();
+            
+            int x = 10, y = 15;
+            Console.WriteLine(nomalCalc.Plus(x, y));
+
+            int res = nomalCalc.Plus(x, y);
+            Console.WriteLine(res);
+
+            // 안됌 nomalCalc.Minus();
+            res = Calc.Minus(x, y);
+            Console.WriteLine(res);
+
+            // 대리자를 사용하는 방식 - 대리자가 대신 실행
+            x = 30; y = 25;
+            Calc delCalc = new Calc();
+            CalcDelegate Callback;
+
+            Callback = new CalcDelegate(delCalc.Plus);
+            int res2 = Callback(x,y); // = Calc.plus() 대신 호출
+            Console.WriteLine(res2);
+            // 호출자가 메서드를 직접 부르지 않는다.
+            // 쓸데 없어보이는데 쓰이는 데가 다 있다.
+            // 배달 앱을 쓰는 이유랑 비슷함.
+
+            // 이게 가능한 이유는 모든 데이터 형태가 같기 때문이다.
+            // 다르면 불가능 함.
+            // 값이 아닌 코드 자체를 매개변수로 넘기고 싶을 때 쓴다.
+            Callback = new CalcDelegate(Calc.Minus);
+            res2 = Callback(x,y);
+            Console.WriteLine(res2);
+            #endregion
+
+            #region
+            int[] Origin = { 4, 7, 8, 2, 9, 1 };
+
+            Console.WriteLine("오름차순 버블 정렬");
+            BubbleSort(Origin, new Compare(AscendCompare));
+
+            foreach (int item in Origin)
+            {
+                Console.Write("{0} ",item);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("내림차순 버블 정렬");
+            BubbleSort(Origin, new Compare(DescendCompare));
+
+            foreach (int item in Origin)
+            {
+                Console.Write("{0} ", item);
+            }
+            Console.WriteLine();
+
+            #endregion
+
+        }
+    }
+}
---
+++ Day07/Day07ConsoleApp/CS27_delegatechain/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS27_delegatechain
+{
+    delegate void ThereIsAFire(string location); // 불 났을 때 대신해주는 대리자
+
+    delegate int Calc(int a, int b); // 
+
+    delegate string Concatenate(string[] args);
+    #region < 클래스 Sample 람다식 프로퍼티 >
+    class Sample
+    {
+        private int valueA; // 맴버변수 - 외부에서 접근 불가
+
+        public int ValueA // 프로퍼티
+        {
+            //get { return valueA; }
+            //set { valueA = value; }
+            get => valueA; // 람다식
+            set => valueA = value; // { valueA = value; } 일반식
+        }
+    }
+    #endregion
+
+    internal class Program
+    {
+        #region < 대리자 함수 >
+        static void Call119(string location)
+        {
+            Console.WriteLine("소방서죠? {0}에 불났어요", location);
+        }
+        static void ShoutOut(string location)
+        {
+            Console.WriteLine("{0}에 불났어요", location);
+        }
+        static void Escape(string location)
+        {
+            Console.WriteLine("{0}에서 탈출합니다.", location);
+        }
+        #endregion
+        static string ProcConcate(string[] args)
+        {
+            string result = string.Empty; // == ""
+            foreach (string s in args)
+            {
+                result += s + "/";
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            #region < 대리자 체인 영역 >
+            /*
+            // 직접 메서드를 다 호출 해야하는 경우
+            var loc = "우리집";
+            Call119(loc);
+            ShoutOut(loc);
+            Escape(loc);
+
+            // 대리자 쓰는 경우
+            // 불이 날 수도 있으니까 미리 준비
+            var otherloc = "경찰서";
+            ThereIsAFire Fire = new ThereIsAFire(Call119);
+            Fire += new ThereIsAFire(ShoutOut);
+            Fire += new ThereIsAFire(Escape); // 대리자에 메서드 추가
+
+            Fire(otherloc);
+
+            Fire -= new ThereIsAFire(ShoutOut); // 대리자에서 메서드 삭제
+
+            Fire("다른집");
+
+            // 익명함수
+            Calc puls = delegate (int a, int b)
+            {
+                return a + b;
+            };
+
+            Console.WriteLine(puls(6,7));
+            // 이렇게도 가능한데 이건 람다식을 알아야 할 수 있다.
+            // 잠시 쓰다 버릴 함수 만들 때 익명 함수를 쓴다.
+            Calc Minus = (a, b) => { return a - b; };
+
+            Console.WriteLine(Minus(67, 9));
+            // (익명)함수를 간결하게 쓰는게 목적
+            Calc simpleMinus = (a, b) => a - b; // 람다식
+            */
+            #endregion
+            Concatenate concat = new Concatenate(ProcConcate);
+            var result = concat(args);
+
+            Console.WriteLine(result);
+
+            Console.WriteLine("람다식으로");
+            Concatenate concat2 = (arr) =>
+            {
+                string res = string.Empty; // == ""
+                foreach (string s in args)
+                {
+                    res += s + "/";
+                }
+                return res;
+            };
+            Console.WriteLine(concat2(args));
+        }
+    }
+}
