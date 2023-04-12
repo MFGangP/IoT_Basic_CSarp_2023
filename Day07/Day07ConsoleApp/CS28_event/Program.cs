dc90fa66315736c83b23ac757d7714accce74723
PARK SEONG HYEON <lovehyun95@gmail.com>
2023-04-12 17:19:21 +0900

    Update Program.cs

--- Day07/Day07ConsoleApp/CS28_event/Program.cs
+++ Day07/Day07ConsoleApp/CS28_event/Program.cs
@@ -8,12 +8,15 @@ namespace CS28_event
 {
     // 이벤트를 사용하려면
     // 1. 대리자를 생성
+    // EventHandler라는 이름을 가진 대리자를 만들껀데 매개 변수로 string을 받을거야
     delegate void EventHandler(string message);
 
     class CustomNotifier 
     {
         // 2. 대리자를 쓰고 싶은 곳에 이벤트를 준비(대리자를 사용한)
+        // 하얀색 EventHandler 델리게이트의 이름 - 이벤트 이름
         public event EventHandler SomethingChanged;
+        // DoSomething이라는 멤버함수 내에 있는 특정 조건을 만족 할 때 이벤트가 발생한다.
         public void DoSomething(int number)
         {
             int temp = number % 10;
@@ -37,7 +40,11 @@ namespace CS28_event
 
         static void Main(string[] args)
         {
+            // CustomNotifier 클래스의 객체 notifier를 동적할당 해서 생성
+            // 그러면 힙 영역에 하나가 생기는거겠지
             CustomNotifier notifier = new CustomNotifier();
+            // 대리자 체인 대리자 하나로 여러개 메소드 동시 참조 가능
+            // notifier 객체의 이벤트가 호출되면 대리자가 CustomHandler를 사용해서 일을 대신 처리해줄거야.
             notifier.SomethingChanged += new EventHandler(CustomHandler);
             
             for (int i = 0; i <= 30; i++)
